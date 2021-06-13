0f2ff0598b2eeda3d34c84cdab64bc8b74149ef5
Aimee Wildstone <11568530+awildstone@users.noreply.github.com>
2021-06-12 21:54:42 -0700

    initial commit

---
+++ ChatUser.js
@@ -0,0 +1,93 @@
+/** Functionality related to chatting. */
+
+// Room is an abstraction of a chat channel
+const Room = require('./Room');
+const URL = "https://catfact.ninja/fact";
+const axios = require("axios");
+
+/** ChatUser is a individual connection from client -> server to chat. */
+
+class ChatUser {
+  /** make chat: store connection-device, rooom */
+
+  constructor(send, roomName) {
+    this._send = send; // "send" function for this user
+    this.room = Room.get(roomName); // room user will be in
+    this.name = null; // becomes the username of the visitor
+
+    console.log(`created chat in ${this.room.name}`);
+  }
+
+  /** send msgs to this client using underlying connection-send-function */
+
+  send(data) {
+    try {
+      this._send(data);
+    } catch {
+      // If trying to send to a user fails, ignore it
+    }
+  }
+
+  /** handle joining: add to room members, announce join */
+
+  handleJoin(name) {
+    this.name = name;
+    this.room.join(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} joined "${this.room.name}".`
+    });
+  }
+
+  /** handle a chat: broadcast to room. */
+
+  async handleChat(text) {
+    if (text === "/members") {
+      this.room.showMembers(this);
+    } else if (text === "/cat") {
+      let res = await this.getCatFact();
+      this.room.broadcast(res);
+    } else {
+      this.room.broadcast({
+        name: this.name,
+        type: 'chat',
+        text: text
+      });
+    } 
+  }
+
+  /** Handle messages from client:
+   *
+   * - {type: "join", name: username} : join
+   * - {type: "chat", text: msg }     : chat
+   */
+
+  handleMessage(jsonData) {
+    let msg = JSON.parse(jsonData);
+    if (msg.type === 'join') this.handleJoin(msg.name);
+    else if (msg.type === 'chat') this.handleChat(msg.text);
+    else throw new Error(`bad message: ${msg.type}`);
+  }
+
+  /** Connection was closed: leave room, announce exit to others */
+
+  handleClose() {
+    this.room.leave(this);
+    this.room.broadcast({
+      type: 'note',
+      text: `${this.name} left ${this.room.name}.`
+    });
+  }
+
+  async getCatFact() {
+    let fact = await axios.get(URL);
+    console.log(fact);
+    return {
+      name: this.name,
+      type: 'cat-fact',
+      text: fact.data.fact
+    }
+  }
+}
+
+module.exports = ChatUser;
